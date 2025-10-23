88587ddb5a3f40d3bd07781ba3eea5a3dff5e483
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-10-23 15:32:56 +0300

    Add loading spinner

--- src/app/pages/articles-list/view/page/articles-list-page.ts
+++ src/app/pages/articles-list/view/page/articles-list-page.ts
@@ -7,6 +7,7 @@ import {ArticlesListStore} from '../../data-access/store/articles-list.store';
 import {injectDispatch} from '@ngrx/signals/events';
 import {articlesListPageEvents} from '../../data-access/store/articles-list.events';
 import {Article} from '../../../../api';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-articles-list-page',
@@ -15,6 +16,7 @@ import {Article} from '../../../../api';
     MatDivider,
     FormsModule,
     SearchBar,
+    MatProgressSpinner,
   ],
   templateUrl: './articles-list-page.html',
   styleUrl: './articles-list-page.scss',
@@ -26,6 +28,7 @@ export class ArticlesListPage implements OnInit {
   private readonly dispatch = injectDispatch(articlesListPageEvents);
 
   protected readonly articles: Signal<Article[]> = this.store.articles;
+  protected readonly isLoading: Signal<boolean> = this.store.isLoading;
 
   ngOnInit() {
     this.dispatch.opened();
